1dd49e1dc5b91093bec27aeec4959e32e56c5816
AkaArya <33246212+AkaArya@users.noreply.github.com>
2021-09-29 14:41:20 +0530

    Add files via upload
    The purpose of this project is to demonstrate ability to collect, work with, and clean a data set. The goal was to prepare tidy data that can be used for later analysis.
    Merges the training and the test sets to create one data set.
    
    Extracts only the measurements on the mean and standard deviation for each measurement. 
    
    Uses descriptive activity names to name the activities in the data set
    
    Appropriately labels the data set with descriptive variable names. 
    
    From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.

---
+++ run_analysis.R
@@ -0,0 +1,10 @@
+x <- c(1,2,3,4,5,1,2,3,1,2,4,5,2,3,1,1,2,3,5,6) # our data set
+mean.result = mean(x) # calculate mean
+print (mean.result)
+
+x <- c(1,2,3,4,5,1,2,3,1,2,4,5,2,3,1,1,2,3,5,6) # our data set
+variance.result = var(x)
+print (variance.result)
+
+sd.result = sqrt(var(x)) # calculate standard deviation
+print (sd.result)
